eb94ff5fa35d38203dbb49e45165d1dd11122d8b
ahmed <ahmed.hsn.404@gmail.com>
2024-08-08 19:46:00 +0600

    updated the project  page as required

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "be90703431386166"; }
+/******/ 		__webpack_require__.h = function() { return "64d86386fd1c1da6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ .next/static/webpack/pages/dark/project.aab3cb196e7160be.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/dark/project",{
+
+/***/ "./src/data/Project/classic.json":
+/*!***************************************!*\
+  !*** ./src/data/Project/classic.json ***!
+  \***************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('[{"id":1,"date":"30 August 2021","title":"Creative advertising in our life became a info noise","image":"/dark/assets/imgs/project/h1.jpg"},{"id":2,"date":"30 August 2021","title":"Creative advertising in our life became a info noise","image":"/dark/assets/imgs/project/h2.jpg"},{"id":3,"date":"30 August 2021","title":"Creative advertising in our life became a info noise","image":"/dark/assets/imgs/project/h3.jpg"},{"id":4,"date":"30 August 2021","title":"Creative advertising in our life became a info noise","image":"/dark/assets/imgs/project/h4.jpg"},{"id":5,"date":"30 August 2021","title":"Creative advertising in our life became a info noise","image":"/dark/assets/imgs/project/h5.jpg"},{"id":6,"date":"30 August 2021","title":"Creative advertising in our life became a info noise","image":"/dark/assets/imgs/project/h6.jpg"}]');
+
+/***/ })
+
+});
